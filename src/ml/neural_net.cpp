da5d8e3fdfca613a35f6b28eb6da91d25edf9b1e
David Nemeskey <nemeskey.david@sztaki.mta.hu>
2013-08-01 16:48:49 +0200

    Debug outputs commented out. 	modified:   ml/neural_net.cpp

--- src/ml/neural_net.cpp
+++ src/ml/neural_net.cpp
@@ -39,13 +39,13 @@ double NeuralNetwork::score_inner(double* const& features,
                                   VectorXd& outputs1) const {
   outputs1 = Map<RowVectorXd>(features, dimensions) * w1.topRows(w1.rows() - 1)
              + w1.bottomRows(1);  // noise
-  for (VectorXd::Index i = 0; i < outputs1.size(); i++) {
-    std::cout << "outputs[" << i << "] == " << outputs1[i] << std::endl;
-  }
+//  for (VectorXd::Index i = 0; i < outputs1.size(); i++) {
+//    std::cout << "outputs[" << i << "] == " << outputs1[i] << std::endl;
+//  }
   outputs1 = outputs1.unaryExpr(afn->act());  // TODO: into the previous expression
-  for (VectorXd::Index i = 0; i < outputs1.size(); i++) {
-    std::cout << "sigma(outputs[" << i << "]) == " << outputs1[i] << std::endl;
-  }
+//  for (VectorXd::Index i = 0; i < outputs1.size(); i++) {
+//    std::cout << "sigma(outputs[" << i << "]) == " << outputs1[i] << std::endl;
+//  }
 
   double y = 0;
   y = outputs1.transpose() * wy.head(hidden_neurons)  // TODO: one line
@@ -107,41 +107,41 @@ void NeuralNetworkGradient::update(double* const& features,
   /* Have to run score() again to fill up the outputs vector... */
   parent.score_inner(features, outputs);
 
-  std::cout << "updating " << features[0] << ", " << features[1] << ", " << features[2] << "..." << std::endl;
-  std::cout << "Outputs ";
-  for (VectorXd::Index i = 0; i < outputs.size(); i++) {
-    std::cout << outputs(i) << " ";
-  }
-//  std::copy(outputs.begin(), outputs.end(),
-//            std::ostream_iterator<double>(std::cout, " "));
-  std::cout << std::endl;
+//  std::cout << "updating " << features[0] << ", " << features[1] << ", " << features[2] << "..." << std::endl;
+//  std::cout << "Outputs ";
+//  for (VectorXd::Index i = 0; i < outputs.size(); i++) {
+//    std::cout << outputs(i) << " ";
+//  }
+////  std::copy(outputs.begin(), outputs.end(),
+////            std::ostream_iterator<double>(std::cout, " "));
+//  std::cout << std::endl;
 
   /* First, let's update the output layer. */
   //double deltay = y * (1 - y);
   double deltay = parent.afn->deriv()(y);
-  std::cout << "deltay == " << deltay << std::endl;
+//  std::cout << "deltay == " << deltay << std::endl;
   double plmdy = parent.learning_rate * mult * deltay;
 
   gradientsy.head(parent.hidden_neurons) += plmdy * outputs;
   gradientsy(parent.hidden_neurons) += plmdy;  // noise
-  std::cout << "Updating wy -= " << parent.learning_rate << " * " << mult
-            << " * " << deltay << " * outputs" << std::endl;
-  print_vec("Gradientsy:", gradientsy);
+//  std::cout << "Updating wy -= " << parent.learning_rate << " * " << mult
+//            << " * " << deltay << " * outputs" << std::endl;
+//  print_vec("Gradientsy:", gradientsy);
 
   //VectorXd deltah = (outputs.array() * (1 - outputs.array())).matrix();
   VectorXd deltah = outputs.unaryExpr(parent.afn->deriv());
-  print_vec("deltah:", deltah);
+//  print_vec("deltah:", deltah);
   /* y'(1) * w(2) -- shouldn't be matched like this, not readable */
   VectorXd deltah_wy = (parent.wy.head(parent.hidden_neurons).array() * deltah.array());
-  print_vec("parent.wy:", parent.wy);
-  print_vec("deltah_wy:", deltah_wy);
-  std::cout << "Updating neurons with lr " << parent.learning_rate
-            << " * deltay " << deltay << " * mult " << mult << std::endl;
+//  print_vec("parent.wy:", parent.wy);
+//  print_vec("deltah_wy:", deltah_wy);
+//  std::cout << "Updating neurons with lr " << parent.learning_rate
+//            << " * deltay " << deltay << " * mult " << mult << std::endl;
   gradients1.topRows(gradients1.rows() - 1) += plmdy *
                 Map<VectorXd>(features, parent.dimensions) * deltah_wy.transpose();
   gradients1.bottomRows(1) += plmdy * deltah_wy.transpose();  // noise
-  std::cout << "delta gradients1: " << std::endl << parent.learning_rate * mult * deltay * Map<VectorXd>(features, parent.dimensions) * deltah_wy.transpose() << std::endl;
-  std::cout << "gradients1: " << std::endl << gradients1 << std::endl;
+//  std::cout << "delta gradients1: " << std::endl << parent.learning_rate * mult * deltay * Map<VectorXd>(features, parent.dimensions) * deltah_wy.transpose() << std::endl;
+//  std::cout << "gradients1: " << std::endl << gradients1 << std::endl;
 }
 
 void NeuralNetworkGradient::update_parent() {

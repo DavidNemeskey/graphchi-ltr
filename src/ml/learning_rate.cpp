d1710fc3f7a84d3cc8daf9117125de44ad3629f8
David Nemeskey <nemeskey.david@sztaki.mta.hu>
2013-08-21 14:21:35 +0200

    Added a flexible learning rate function framework. 	new file:   ml/learning_rate.cpp 	new file:   ml/learning_rate.h

---
+++ src/ml/learning_rate.cpp
@@ -0,0 +1,133 @@
+#include "ml/learning_rate.h"
+
+#include <cstdio>     // sscanf
+#include <algorithm>  // copy_if, for_each
+#include <iterator>   // back_inserter
+#include <sstream>    // istringstream
+
+#include <iostream>
+
+LearningRate::LearningRate(double learning_rate_)
+  : learning_rate(learning_rate_) {}
+
+ConstantLearningRate::ConstantLearningRate(double learning_rate_)
+    throw (std::invalid_argument) : LearningRate(learning_rate_) {
+  if (learning_rate <= 0) {
+    throw (std::invalid_argument("learning_rate must be greater than 0"));
+  }
+}
+
+LinearLearningRate::LinearLearningRate(double starting_value_, double decrease_,
+                                       double ending_value_)
+    throw (std::invalid_argument) : LearningRate(starting_value_),
+                                    start_point(starting_value_),
+                                    decrease(decrease_),
+                                    end_point(ending_value_) {
+  if (starting_value_ <= 0) {
+    throw (std::invalid_argument("starting_value must be greater than 0"));
+  }
+  if (ending_value_ < 0) {
+    throw (std::invalid_argument("ending_value must not be less than 0"));
+  }
+  if (ending_value_ >= starting_value_) {
+    throw (std::invalid_argument(
+          "ending_value must be less than starting_value"));
+  }
+}
+
+bool LinearLearningRate::advance() {
+  learning_rate -= decrease;
+  if (learning_rate < end_point) {
+    learning_rate = end_point;
+    return false;
+  } else {
+    return true;
+  }
+}
+
+void LinearLearningRate::reset() {
+  learning_rate = start_point;
+}
+
+CompositeLearningRate::CompositeLearningRate(std::vector<LearningRate*> parts_)
+    throw (std::invalid_argument) : index(0) {
+  std::copy_if(parts_.begin(), parts_.end(), std::back_inserter(parts),
+               [] (LearningRate* p) { return p != NULL; });
+  if (parts.size() == 0) {
+    throw (std::invalid_argument("no valid learning rate functions specified"));
+  }
+  learning_rate = parts[index]->get();
+}
+
+bool CompositeLearningRate::advance() {
+  if (index >= parts.size()) {
+    return false;
+  } else {
+    bool ret = parts[index]->advance();
+    learning_rate = parts[index]->get();
+    if (!ret) {
+      index++;
+    }
+    return true;
+  }
+}
+
+void CompositeLearningRate::reset() {
+  std::for_each(parts.begin(), parts.end(), std::mem_fun(&LearningRate::reset));
+  index = 0;
+  learning_rate = parts[index]->get();
+}
+
+LearningRate* create_learning_rate_function(const std::string& reflection_name)
+    throw (std::invalid_argument) {
+  std::istringstream ss(reflection_name);
+  std::string name;
+  std::getline(ss, name, ':');
+  if (name == "LinearLearningRate" || name == "linear") {
+    std::string params;
+    std::getline(ss, params, '\n');
+    double start = 0;
+    double step = 0;
+    double end = 0;
+    sscanf(params.c_str(), "%lf:%lf:%lf", &start, &step, &end);
+    return new LinearLearningRate(start, step, end);
+  } else if (name == "ConstantLearningRate" || name == "constant") {
+    double c = 0;
+    ss >> c;
+    return new ConstantLearningRate(c);
+  } else if (name == "CompositeLearningRate" || name == "composite") {
+    std::vector<LearningRate*> parts;
+    while (ss.good()) {
+      std::getline(ss, name, ';');
+      if (!ss.fail() && !ss.bad()) {
+        parts.push_back(create_learning_rate_function(name));
+      }
+    }
+    return new CompositeLearningRate(parts);
+  } else {
+    return NULL;
+  }
+}
+
+//int main(int argc, char* argv[]) {
+//  std::vector<LearningRate*> v;
+////  v.push_back(new LinearLearningRate(0.9, 0.1, 0.15));
+//  //v.push_back(create_learning_rate_function("linear:0.9:0.1:0.15"));
+////  v.push_back(new LinearLearningRate(0.9, 0.2, 0.15));
+////  v.push_back(create_learning_rate_function("constant:0.5"));
+//  //v.push_back(new ConstantLearningRate(0.5));
+////  CompositeLearningRate clr(v);
+//  //LearningRate* clr = create_learning_rate_function("composite:linear:0.9:0.1:0.15;constant:0.5");
+//  LearningRate* clr = create_learning_rate_function("composite:linear:0.9:0.1:0.15;linear:0.9:0.1:0.15");
+//  for (size_t i = 0; i < 15; i++) {
+//    std::cout << "Learning rate " << clr->get() << std::endl;
+//    if (!clr->advance()) break;
+//  }
+//  clr->reset();
+//  std::cout << std::endl;
+//  for (size_t i = 0; i < 15; i++) {
+//    std::cout << "Learning rate " << clr->get() << std::endl;
+//    if (!clr->advance()) break;
+//  }
+//}
+
---
+++ src/ml/learning_rate.h
@@ -0,0 +1,140 @@
+#ifndef LEARNING_RATE_H
+#define LEARNING_RATE_H
+/**
+ * @file
+ * @author  David Nemeskey
+ * @version 0.1
+ *
+ * @section LICENSE
+ *
+ * Copyright [2013] [MTA SZTAKI]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Learning rate strategies.
+ *
+ * @warning This file uses features introduced in the C++11 revision of the
+ *          standard. Use a compatible compiler with the appropriate compilation
+ *          options (e.g. -std=c++0x or -std=c++11 for GCC).
+ */
+
+#include <stdexcept>
+#include <vector>
+#include <string>
+
+/** The ancestor of all learning rate strategies. */
+class LearningRate {
+public:
+  LearningRate(double learning_rate=1);
+
+  /** Returns the current value of the learning rate function. */
+  inline double get() const { return learning_rate; }
+
+  /**
+   * Advances the function to the next point. Usually called after an
+   * iteration. Subclasses must override this method to suit their needs.
+   *
+   * @return true if we didn't reach the end of the domain; @c false otherwise.
+   *              The learning rate IS changed the first time this function
+   *              returns @c false; after that, it should remain constant.
+   */
+  virtual bool advance()=0;
+
+  /** Resets the object to its starting state. */
+  virtual void reset()=0;
+
+protected:
+  /** The current value of the learning rate function. */
+  double learning_rate;
+};
+
+/** A constant learning rate. */
+class ConstantLearningRate : public LearningRate {
+public:
+  /** @throws std::invalid_argument if not <tt>0 < learning_rate</tt>. */
+  ConstantLearningRate(double learning_rate) throw (std::invalid_argument);
+
+  inline bool advance() { return true; }
+  inline void reset() {};
+};
+
+/** Linearly decreasing learning rate in an interval. */
+class LinearLearningRate : public LearningRate {
+public:
+  /**
+   * Constructor.
+   *
+   * @param starting_value the start point of the interval.
+   * @param decrease the step size, subtracted from the learning rate at each
+   *                 step.
+   * @param ending_value the end point of the interval.
+   * @throws std::invalid_argument if @p starting_value or @p ending_value is
+   *                               less than @c 0.
+   */
+  LinearLearningRate(double starting_value, double decrease,
+                     double ending_value) throw (std::invalid_argument);
+
+  bool advance();
+  void reset();
+
+private:
+  /** The start point of the interval. */
+  double start_point;
+  /** The learning rate is decreased by this amount every step. */
+  double decrease;
+  /** The end point of the interval. */
+  double end_point;
+};
+
+/**
+ * Executes several learning rate strategies in succession: the first one is
+ * followed until its @c advance() method returns @c false, then the second one,
+ * and so on.
+ */
+class CompositeLearningRate : public LearningRate {
+public:
+  /**
+   * Constructor.
+   *
+   * @param parts the learning rate functions we iterate through.
+   * @throws std::invalid_argument if parts is empty or contains only @c NULLs.
+   */
+  CompositeLearningRate(std::vector<LearningRate*> parts)
+    throw (std::invalid_argument);
+
+  bool advance();
+  void reset();
+
+private:
+  /** The strategies we iterate through. */
+  std::vector<LearningRate*> parts;
+  /** The index of the currently active strategy. */
+  size_t index;
+};
+
+/**
+ * A "reflection" method: creates a learning rate object by name.
+ * @p reflection_name must be in the format
+ * <tt>learning_rate_function [:param]* </tt>, where the <tt>param</tt>s are
+ * the parameters to the learning rate strategy's constructor. In addition,
+ * CompositeLearningRate separates its parts by <tt>;</tt>.
+ *
+ * @note This function does not handle nest CompositeLearningRate definitions.
+ *
+ * @throws std::invalid_argument if the constructor of the learning rate
+ *                               strategy to be instantiated does.
+ */
+LearningRate* create_learning_rate_function(const std::string& reflection_name)
+  throw (std::invalid_argument);
+
+#endif

3d121eb96a244d8c14a2f1e58f640be585b54cce
David Nemeskey <nemeskey.david@sztaki.mta.hu>
2013-10-08 17:09:59 +0200

    regression_tree compiles. 	modified:   ml/regression_tree.cpp 	modified:   ml/regression_tree.h

--- src/ml/regression_tree.cpp
+++ src/ml/regression_tree.cpp
@@ -1,5 +1,6 @@
 #include "ml/regression_tree.h"
-#include <Eigen/Constants>
+#include "ml/learning_rate.h"
+//#include <Eigen/util/Constants.h>
 
 using Eigen::Map;
 using Eigen::RowVectorXd;
@@ -22,22 +23,21 @@ RegressionTree::~RegressionTree() {
 void RegressionTree::read_data_item(double* const& features, double& output) {
   /* Expand the data matrix. */
   if (data.cols() == cols) {
-    data.conservativeResize(NoChange, 2 * data.cols());
+    data.conservativeResize(Eigen::NoChange, 2 * data.cols());
     outputs.conservativeResize(2 * outputs.size());
   }
 
   data.row(cols) = Map<RowVectorXd>(features, dimensions);
-  outputs(cols) = cls;
+  outputs(cols) = output;
   cols++;
 }
 
 void RegressionTree::finalize_data() {
-  data.conservativeResize(NoChange, cols);
-  classes.conservativeResize(cols);
+  data.conservativeResize(Eigen::NoChange, cols);
+  outputs.conservativeResize(cols);
 }
 
-/** Creates the @c sorted array. */
-void create_sorted() {
+void RegressionTree::create_sorted() {
   /* First, create the sorted array, ... */
   sorted.resize(data.rows(), data.cols());
   ArrayXi tmp = ArrayXi::Constant(data.cols(), 0);
@@ -59,64 +59,116 @@ void create_sorted() {
  * @param[in] delta if the error does not decrease by at least @p delta, stop.
  * @param[in] q if one of the children would have at most q nodes, stop.
  */
-void build_tree(double delta, size_t q) {
+void RegressionTree::build_tree(double delta, size_t q) {
   create_sorted();
-  tree = new RealNode();
+  tree = new RealNode(0);
   tree->output = outputs.sum() / outputs.size();
-  tree->error  = (outputs - tree->output).pow(2);
-  split_node(tree, delta, q);
+  tree->error  = (outputs - tree->output).pow(2).sum();
+  ArrayXi valid = ArrayXi::Ones(sorted.rows());
+  int max_id = 0;
+  split_node(tree, valid, max_id, valid.size(), delta, q);
 }
 
-/**
- * Recursively splits the nodes in the tree.
- */
-void split_node(RealNode* node, double delta, size_t q) {
+void RegressionTree::split_node(Node* node, ArrayXi& valid, int& max_id,
+                                ArrayXi::Index num_docs, double delta, size_t q) {
   double min_error   = node->error;
   size_t min_feature = dimensions;
   double min_value   = 0;
+  double min_left_error  = 0;
+  double min_right_error = 0;
+  ArrayXd::SegmentReturnType min_head = outputs.head(0);  // placeholder only
+  ArrayXd::SegmentReturnType min_tail = outputs.tail(0);  // placeholder only
+  size_t min_left_valids = 0;
 
-  ArrayXd sorted_outputs(outputs.size());
+  // TODO: to argument?
+  /* Create a list of the outputs. */
+  ArrayXd sorted_outputs(num_docs);
 
   /* Iterate through all features. */
   for (size_t f = 0; f < dimensions; f++) {
-    // TODO: resort after each 
     /* The outputs sorted by the current feature. */
-    for (size_t i = 0; i < sorted_outputs.size(); i++) {
-      sorted_outputs(i) = outputs(sorted(i, f));
+    for (size_t i = 0, j = 0; j < sorted_outputs.size(); i++) {
+      if (valid(sorted(i, f)) == node->id) sorted_outputs(j++) = outputs(sorted(i, f));
     }
 
+    /* Number of valids on the left side of the split. */
+    size_t left_valids = 0;
     /* All possible split with that feature. */
-    for (size_t split = q; split < sorted.rows() - q; split++) {
-      size_t anti_split = sorted.rows() - split;
-      auto head = sorted_outputs.head(split);
-      auto tail = sorted_outputs.tail(sorted.rows() - split);
-      double curr_error = (head - split).pow(2) / split +
-                          (tail - anti_split).pow(2) / anti_split;
-      if (curr_error < min_error) {
-        min_error   = curr_error;
-        min_feature = f;
-        min_value   = data(sorted(split, f), f);
+    for (size_t split = 0; split < sorted.rows() - q; split++) {
+      if (valid(sorted(split, f)) != node->id) continue;  // Document not under this node
+
+      if (left_valids >= q) {
+        auto head = sorted_outputs.head(left_valids);
+        auto tail = sorted_outputs.tail(num_docs - left_valids);
+        double left_error = (head - head.sum() / head.size()).pow(2).sum();
+        double right_error = (tail - tail.sum() / tail.size()).pow(2).sum();
+        double curr_error = left_error + right_error;
+        if (curr_error < min_error) {
+          min_error   = curr_error;
+          min_feature = f;
+          min_value   = data(sorted(split, f), f);
+          min_left_error = left_error;
+          min_right_error = right_error;
+          min_head = head;
+          min_tail = tail;
+          min_left_valids = left_valids;
+        }
       }
+
+      left_valids++;
     }
   }
 
   if (min_error + delta < node->error) {
-    // TODO: split
+    node->feature_no  = min_feature;
+    // TODO: handle enum node type
+    ((RealNode*)node)->feature_val = min_value;
+
+    int left_id  = ++max_id;
+    int right_id = ++max_id;
+    ArrayXi::Index left_docs  = 0;
+    ArrayXi::Index right_docs = 0;
+    // TODO: sparse / set / map
+    for (ArrayXXi::Index i = 0; i < valid.size(); i++) {
+      if (valid(i) == node->id) {
+        if (data(i, min_feature) < min_value) {
+          valid(i) = left_id;
+          left_docs++;
+        } else {
+          valid(i) = right_id;
+          right_docs++;
+        }
+      }
+    }  // for
+
+    node->left  = new RealNode(left_id);
+    node->left->output = min_head.sum() / min_head.size();
+    node->left->error = min_left_error;
+    node->right = new RealNode(right_id);
+    node->right->output = min_tail.sum() / min_tail.size();
+    node->right->error = min_right_error;
+
+    split_node(node->left, valid, max_id, left_docs, delta, q);
+    split_node(node->right, valid, max_id, right_docs, delta, q);
   }
 }
+
+RegressionTree::Node::Node(int id_) : id(id_) {}
 
 bool RegressionTree::Node::is_leaf() const {
   return left == NULL;
 }
 
-bool RegressionTree::Node::~Node() {
+RegressionTree::Node::~Node() {
   if (left != NULL) {
     delete left;
     delete right;
   }
 }
 
-RegressionTree::Comp::Comp(ArrayXXd& data_, ArrayXXd::Index column_)
+RegressionTree::RealNode::RealNode(int id_) : Node(id_) {}
+
+RegressionTree::Comp::Comp(const ArrayXXd& data_, ArrayXXd::Index column_)
   : data(data_), column(column_) {}
 
 bool RegressionTree::Comp::operator() (int i, int j) const {
--- src/ml/regression_tree.h
+++ src/ml/regression_tree.h
@@ -35,12 +35,15 @@ using Eigen::VectorXd;
 using Eigen::ArrayXXd;
 using Eigen::ArrayXd;
 using Eigen::ArrayXXi;
+using Eigen::ArrayXi;
 
 // TODO: Create in-memory model ancestor class
 class RegressionTree {
 protected:
   /** A node in the tree; ancestor class. */
   struct Node {
+    /** The node id. */
+    int id;
     /** The index of the feature we test in this node. */
     size_t feature_no;
     /**
@@ -51,8 +54,10 @@ protected:
     /** The LMSE error of the node. */
     double error;
     /** The sibling nodes. */
-    struct Node* left, right;
+    struct Node* left;
+    struct Node* right;
 
+    Node(int id);
     /** Deletes the whole tree under the current node. */
     ~Node();
     /*
@@ -69,12 +74,14 @@ protected:
   struct RealNode : public Node {
     /** The feature value we test against. */
     double feature_val;
+
+    RealNode(int id);
   };
 
   /** Comparator for build_tree. */
   struct Comp {
-    ArrayXXd::Index column;
     const ArrayXXd& data;
+    ArrayXXd::Index column;
 
     Comp(const ArrayXXd& data, ArrayXXd::Index column=0);
     /**
@@ -99,6 +106,13 @@ public:
   /** Finalizes the data; to be called after all data points have been read. */
   void finalize_data();
 
+  /**
+   * Builds the tree.
+   * @param[in] delta if the error does not decrease by at least @p delta, stop.
+   * @param[in] q if one of the children would have at most q nodes, stop.
+   */
+  void build_tree(double delta, size_t q);
+
   // TODO: does this model need gradients at all? I don't think so.
 
   double score(double* const& features) const;
@@ -107,6 +121,20 @@ public:
   std::string str() const;
 
 private:
+  /** Creates the @c sorted array. */
+  void create_sorted();
+
+//  /**
+//   * Recursively splits the nodes in the tree.
+//   *
+//   * @param[in,out] valid an index of the valid rows -- each needs its won valid
+//   *                      vector.
+//   * @param[in,out] max_id the maximum node id thus far.
+//   * @param[in] num_docs the number of documents under the current node.
+//   */
+  void split_node(Node* node, ArrayXi& valid, int& max_id,
+                  ArrayXi::Index num_docs, double delta, size_t q);
+
   /** Dimensions of the feature space. */
   size_t dimensions;
   /** The learning rate function. */

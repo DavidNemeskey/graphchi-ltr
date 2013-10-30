f8df2ffd80482311fbc64450d3a9af645ad8712f
David Nemeskey <nemeskey.david@sztaki.mta.hu>
2013-10-30 14:15:30 +0100

    Non-graphchi version of the nDCG optimizer in lambda_optimizers.hpp. 	new file:   ndcg_optimizer.cpp 	new file:   ndcg_optimizer.h

---
+++ src/ndcg_optimizer.cpp
@@ -0,0 +1,72 @@
+#include "ndcg_optimizer.h"
+
+#include <iostream>
+
+const double RealNdcgOptimizer::log_2 = log(2);
+
+void RealNdcgOptimizer::compute_idcg(ArrayXd relevance) {
+  std::sort(relevance.data(), relevance.data() + relevance.size(),
+            std::greater<double>());
+  idcg = 0;
+  for (ArrayXd::Index i = 0; i < relevance.size(); i++) {
+    idcg += dcg_at_i(relevance, i, i);
+  }
+}
+
+void RealNdcgOptimizer::rankings(const ArrayXd& outputs) {
+  /* First, we sort the outputs, and create a rank -> document index mapping. */
+  std::vector<ArrayXd::Index> sorted(outputs.size());
+  for (size_t i = 0; i < sorted.size(); i++) {
+    sorted[i] = i;
+  }
+  std::sort(sorted.begin(), sorted.end(),
+            [&outputs](const ArrayXd::Index& i, const ArrayXd::Index& j) -> bool
+              {return outputs[i] > outputs[j]; });
+
+  /* Now the inverse: the document index -> rank mapping. */
+  // TODO: isn't there a simpler way?
+  ranking_order.resize(outputs.size());
+  for (size_t i = 0; i < sorted.size(); i++) {
+    ranking_order[sorted[i]] = i;
+  }
+}
+
+double RealNdcgOptimizer::delta(
+    const ArrayXd& relevance, ArrayXd::Index i, ArrayXd::Index j) {
+  double ret = 0;
+  ret = - dcg_at_i(relevance, i, ranking_order[i]) 
+        - dcg_at_i(relevance, j, ranking_order[j])
+        + dcg_at_i(relevance, i, ranking_order[j])
+        + dcg_at_i(relevance, j, ranking_order[i]);
+  return ret / idcg;
+}
+
+double RealNdcgOptimizer::compute_ndcg(const ArrayXd& relevance) {
+  double dcg = 0;
+  for (size_t i = 0; i < ranking_order.size(); i++) {
+    dcg += dcg_at_i(relevance, i, ranking_order[i]);
+  }
+  return dcg / idcg;
+}
+
+inline double RealNdcgOptimizer::dcg_at_i(
+    const ArrayXd& relevance, ArrayXd::Index i, ArrayXd::Index rank) {
+  return (pow(2, relevance(i)) - 1) / (log(rank + 2) / log_2);
+}
+
+int main(int argc, char* argv[]) {
+  RealNdcgOptimizer r;
+  ArrayXd vr(4);
+  vr << 3, 2, 4, 1;
+  r.compute_idcg(vr);
+  std::cout << r.idcg << std::endl;
+  ArrayXd vo(4);
+//  vo << 3, 2, 1, 4;
+  vo << 3, 2, 1, 4;
+  r.rankings(vo);
+  for (size_t i = 0; i < r.ranking_order.size(); i++) {
+    std::cout << r.ranking_order[i] << std::endl;
+  }
+  std::cout << "ndcg " << r.compute_ndcg(vr) << std::endl;
+  std::cout << "delta " << r.delta(vr, 2, 3) << std::endl;
+}
---
+++ src/ndcg_optimizer.h
@@ -0,0 +1,70 @@
+#pragma once
+/**
+ * @file
+ * @author  David Nemeskey
+ * @version 0.1
+ *
+ * @section LICENSE
+ *
+ * Copyright [2013] [MTA SZTAKI]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Information retrieval measure optimizers for the LambdaXXX models.
+ * @todo This is the third Ndcg computer -- merge with the other two.
+ */
+
+#include <vector>
+#include <Eigen/Dense>
+
+using Eigen::ArrayXd;
+
+/**
+ * The order the functions must be called:
+ * 1. compute_idcg
+ * 2. rankings
+ * 3. delta or compute_ndcg
+ */
+class RealNdcgOptimizer {
+public:
+  /** Computes the ideal DCG, required for nDCG, and stores it. */
+  void compute_idcg(ArrayXd relevance);
+
+  /** Sorts the documents according to their rankings. */
+  void rankings(const ArrayXd& outputs);
+
+  /**
+   * Returns the delta in the nDCG score if document @p i and @p j change
+   * places in the ranking.
+   */
+  double delta(const ArrayXd& relevance, ArrayXd::Index i, ArrayXd::Index j);
+
+  /** Computes the nDCG. */
+  double compute_ndcg(const ArrayXd& relevance);
+
+//private:
+  /**
+   * Computes the contribution of document @p i in the DCG score at rank
+   * @p rank.
+   */
+  inline double dcg_at_i(const ArrayXd& relevance,
+                         ArrayXd::Index i, ArrayXd::Index rank);
+  /** The value of log(2), pre-computed. */
+  static const double log_2;
+  /** The ideal DCG. */
+  double idcg;
+  /** The document indices in their ranking order. */
+  /** A document index -> rank map. */
+  std::vector<ArrayXd::Index> ranking_order;
+};
+
